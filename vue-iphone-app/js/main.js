199ab430eb3bf30ed7a955d25ea906fe16d6bdca
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-01 14:53:38 +0100

    in/out stock

--- vue-iphone-app/js/main.js
+++ vue-iphone-app/js/main.js
@@ -4,6 +4,8 @@ const myApp = Vue.createApp({
             product: "iPhone 13",
             description: "iPhone 13 is the latest iPhone in the iPhone series. It is powered by a 2.84-GHz dual-core processor and has a camera system that can take up to 8 photos in a single burst.",
             image: './assets/images/iphone-13-blue.png',
+            inStock: true,
+            // quantity: 6
         }
     }
 })
