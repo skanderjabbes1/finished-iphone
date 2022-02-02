ae80fef486a1df6a35b3fca29f298caa8fdf887f
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-02 15:39:38 +0100

    add some comments

--- vue-iphone-app/js/main.js
+++ vue-iphone-app/js/main.js
@@ -4,9 +4,13 @@ import navbarComponent from './components/NavbarComponent.js'
 import cartComponent from './components/CartComponent.js'
 import productComponent from './components/ProductComponent.js'
 
+// Vue application
 const myApp = Vue.createApp(app);
 
+// Components
 myApp.component('navbar-component', navbarComponent);
 myApp.component('cart-component', cartComponent);
 myApp.component('product-component', productComponent);
+
+// Mount the application
 myApp.mount('#app');
