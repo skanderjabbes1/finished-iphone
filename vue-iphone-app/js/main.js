6327ca75f9c7a2f19e7056c18daa12df12e40735
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-02 15:38:52 +0100

    export main app to a separate file

--- vue-iphone-app/js/main.js
+++ vue-iphone-app/js/main.js
@@ -1,14 +1,11 @@
+import app from './app.js';
+
 import navbarComponent from './components/NavbarComponent.js'
 import cartComponent from './components/CartComponent.js'
 import productComponent from './components/ProductComponent.js'
 
-const myApp = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-        }
-    },
-})
+const myApp = Vue.createApp(app);
+
 myApp.component('navbar-component', navbarComponent);
 myApp.component('cart-component', cartComponent);
 myApp.component('product-component', productComponent);
