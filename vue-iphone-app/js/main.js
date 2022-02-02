70d173ba6c9c92a66284bb4964d5ddabfebe720a
jbeli basma <jbelibasma22@gmail.com>
2022-02-02 11:44:24 +0100

    create dbutton selon instock

--- vue-iphone-app/js/main.js
+++ vue-iphone-app/js/main.js
@@ -4,20 +4,31 @@ const myApp = Vue.createApp({
             cart: 0,
             product: "iPhone 13",
             description: "iPhone 13 is the latest iPhone in the iPhone series. It is powered by a 2.84-GHz dual-core processor and has a camera system that can take up to 8 photos in a single burst.",
-            image: './assets/images/iphone-13-blue.png',
-            inStock: true,
-            // quantity: 6,
+            // image: './assets/images/iphone-13-blue.png',
+            // inStock: true,
+             
             specs: ['5G speed', 'Edge-to-edge OLED display', 'Ceramic Shield'],
             variants: [
-                { id: 'iphone13-001', color: 'blue', image: './assets/images/iphone-13-blue.png' },
-                { id: 'iphone13-002', color: 'black', image: './assets/images/iphone-13-midnight.png' },
-                { id: 'iphone13-003', color: 'red', image: './assets/images/iphone-13-red.png' },
+                { id: 'iphone13-001',quantity: 1, color: 'blue', image: './assets/images/iphone-13-blue.png' },
+                { id: 'iphone13-002',quantity: 6, color: 'black', image: './assets/images/iphone-13-midnight.png' },
+                { id: 'iphone13-003',quantity: 0, color: 'red', image: './assets/images/iphone-13-red.png' },
             ],
+            activeVariant:0
+          
         }
     },
-    methods: {
-        updateImage(imageUrl) {
-            this.image = imageUrl
+    // methods: {
+    //     updateImage(imageUrl) {
+    //         this.image = imageUrl
+    //     }
+    // },
+    computed: {
+        instock() {
+            return this.variants[this.activeVariant].quantity
+        },
+        image(){
+            return this.variants[this.activeVariant].image
+
         }
     }
 })
