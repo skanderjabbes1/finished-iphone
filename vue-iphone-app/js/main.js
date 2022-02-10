54c4bf06886f78f51ef34b1354c144f3b769c5f9
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-10 09:58:26 +0100

    regitser review-zone-component

--- vue-iphone-app/js/main.js
+++ vue-iphone-app/js/main.js
@@ -3,14 +3,16 @@ import app from './app.js';
 import navbarComponent from './components/NavbarComponent.js'
 import cartComponent from './components/CartComponent.js'
 import productComponent from './components/ProductComponent.js'
+import reviewZoneComponent from './components/review/ReviewZoneComponent.js';
 
 // Vue application
 const myApp = Vue.createApp(app);
 
-// Components
+// Components registration
 myApp.component('navbar-component', navbarComponent);
 myApp.component('cart-component', cartComponent);
 myApp.component('product-component', productComponent);
+myApp.component('review-zone-component', reviewZoneComponent);
 
 // Mount the application
 myApp.mount('#app');
