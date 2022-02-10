c085650fa1982062ac3143b9bee96b32017903e9
Houcem Hedhly <hedhoucem@gmail.com>
2022-02-10 09:56:50 +0100

    create reviewZoneComponent

---
+++ vue-iphone-app/js/components/review/ReviwZoneComponent.js
@@ -0,0 +1,10 @@
+const reviewZoneComponent = {
+    template: `
+        <div class="review-zone">
+            <h2>ReviewFormComponent</h2>
+            <h2>ReviewListComponent</h2>
+        </div>
+        `
+}
+
+export default reviewZoneComponent
